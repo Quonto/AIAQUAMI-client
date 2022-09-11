d025b59bbaf610062e5d97892f12592b86c852cc
Quonto <zikica.pet95@gmail.com>
2022-09-11 13:03:12 +0200

    Faza

--- src/Component/register/Register.js
+++ src/Component/register/Register.js
@@ -1,16 +1,37 @@
+import { useState } from "react";
 const Register = () => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    institution: "",
+    domain: "Academy",
+    intendedUse: "Informative",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleRegister = () => {
+    console.log(user);
+  };
+
   return (
     <div className="register">
-      {" "}
       <h4>Create a new account.</h4>
       <hr />
       <div className="form-group">
         <label htmlFor="firstname" className="col-md-2 control-label">
-          {" "}
           FirstName{" "}
         </label>
         <div className="col-md-10">
-          <input id="firstname" type="text" className="form-control" />
+          <input
+            id="firstname"
+            type="text"
+            className="form-control"
+            onChange={(e) => {
+              setUser({ ...user, firstName: e.target.value });
+            }}
+          />
         </div>
       </div>
       <div className="form-group">
@@ -18,16 +39,29 @@ const Register = () => {
           LastName
         </label>
         <div className="col-md-10">
-          <input id="lastname" type="text" className="form-control" />
+          <input
+            id="lastname"
+            type="text"
+            className="form-control"
+            onChange={(e) => {
+              setUser({ ...user, lastName: e.target.value });
+            }}
+          />
         </div>
       </div>
       <div className="form-group">
         <label htmlFor="institution" className="col-md-2 control-label">
           Institution
         </label>
-
         <div className="col-md-10">
-          <input type="text" className="form-control" id="institution" />
+          <input
+            type="text"
+            className="form-control"
+            id="institution"
+            onChange={(e) => {
+              setUser({ ...user, institution: e.target.value });
+            }}
+          />
         </div>
       </div>
       <div className="form-group">
@@ -35,7 +69,13 @@ const Register = () => {
           Domain
         </label>
         <div className="col-md-10">
-          <select id="selectItemList" className="form-control">
+          <select
+            id="selectItemList"
+            className="form-control"
+            onChange={(e) => {
+              setUser({ ...user, domain: e.target.value });
+            }}
+          >
             <option value="Academy">Academy</option>
             <option value="Industry">Industry</option>
             <option value="NGO">NGO</option>
@@ -48,7 +88,13 @@ const Register = () => {
           Intended Use
         </label>
         <div className="col-md-10">
-          <select id="intendeduse" className="form-control">
+          <select
+            id="intendeduse"
+            className="form-control"
+            onChange={(e) => {
+              setUser({ ...user, intendedUse: e.target.value });
+            }}
+          >
             <option value="Informative">Informative</option>
             <option value="For education">For education</option>
             <option value="Professional">Professional</option>
@@ -62,7 +108,14 @@ const Register = () => {
           Email
         </label>
         <div className="col-md-10">
-          <input id="email" type="text" className="form-control" />
+          <input
+            id="email"
+            type="text"
+            className="form-control"
+            onChange={(e) => {
+              setUser({ ...user, email: e.target.value });
+            }}
+          />
         </div>
       </div>
       <div className="form-group">
@@ -70,7 +123,14 @@ const Register = () => {
           Password
         </label>
         <div className="col-md-10">
-          <input id="password" type="password" className="form-control" />
+          <input
+            id="password"
+            type="password"
+            className="form-control"
+            onChange={(e) => {
+              setUser({ ...user, password: e.target.value });
+            }}
+          />
         </div>
       </div>
       <div className="form-group">
@@ -83,12 +143,18 @@ const Register = () => {
             id="confirmpassword"
             type="password"
             className="form-control"
+            onChange={(e) => {
+              setUser({ ...user, confirmPassword: e.target.value });
+            }}
           />
         </div>
       </div>
       <div className="form-group">
         <div className="col-md-offset-2 col-md-10">
-          <input type="submit" className="btn btn-default" value="Register" />
+          <button className="btn btn-default" onClick={handleRegister}>
+            {" "}
+            Register
+          </button>
         </div>
       </div>
     </div>
